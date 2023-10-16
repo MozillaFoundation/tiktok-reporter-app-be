1808df0d02ccdba3c6005049acc9db697b623d7f
Mircea Matei <mircea.matei@thoughtworks.com>
2023-10-16 18:00:34 +0300

    TEST GithubActions 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DataBaseModule } from './database/database.module';
 import { StudiesModule } from './studies/studies.module';
 
+// TEST GithubActions 1
 @Module({
   imports: [
     ConfigModule.forRoot({
