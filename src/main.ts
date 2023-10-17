7f0206e3272ff255061471e1a9d14bf91856ee9e
MirceaMateiMozilla <mircea@mozillafoundation.org>
2023-10-17 17:59:45 +0300

    Changed port settings

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
+const port = process.env.PORT || 8080;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -19,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
