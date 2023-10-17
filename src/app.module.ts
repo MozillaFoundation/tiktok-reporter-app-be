303a783b166882d25cdfaf348e6de5db68d32ccb
MirceaMateiMozilla <mircea@mozillafoundation.org>
2023-10-17 19:03:10 +0300

    TEST Cloud Build 8

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,31 @@
+import { Module, ValidationPipe } from '@nestjs/common';
+
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DataBaseModule } from './database/database.module';
+import { StudiesModule } from './studies/studies.module';
 
-// TEST Cloud Build 7
+// TEST Cloud Build 8
 @Module({
-  // imports: [
-  //   ConfigModule.forRoot({
-  //     isGlobal: true,
-  //     envFilePath: `.env.${process.env.NODE_ENV}`,
-  //   }),
-  //   DataBaseModule,
-  //   StudiesModule,
-  // ],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    DataBaseModule,
+    StudiesModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
-    // {
-    //   provide: APP_PIPE,
-    //   useValue: new ValidationPipe({
-    //     whitelist: true,
-    //   }),
-    // },
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
   ],
 })
 export class AppModule {}
