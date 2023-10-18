406bcd1730c43dbc95691e9f4247f325eca116ae
MirceaMateiMozilla <mircea@mozillafoundation.org>
2023-10-18 09:23:58 +0300

    TEST Cloud Build 10

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,31 @@
+import { Module, ValidationPipe } from '@nestjs/common';
+
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DataBaseModule } from './database/database.module';
+import { StudiesModule } from './studies/studies.module';
 
-// TEST Cloud Build 9
+// TEST Cloud Build 10
 @Module({
-  // imports: [
-  //   ConfigModule.forRoot({
-  //     isGlobal: true,
-  //     envFilePath: `.env.${process.env.NODE_ENV}`,
-  //   }),
-  //   DataBaseModule,
-  //   StudiesModule,
-  // ],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    DataBaseModule,
+    StudiesModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
-    // {
-    //   provide: APP_PIPE,
-    //   useValue: new ValidationPipe({
-    //     whitelist: true,
-    //   }),
-    // },
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
   ],
 })
 export class AppModule {}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,15 +9,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       useFactory: (config: ConfigService) => {
         return {
           type: 'postgres',
-          host: config.getOrThrow<string>('PG_HOST'),
-          port: config.getOrThrow<number>('PG_PORT'),
-          database: config.getOrThrow<string>('PG_DATABASE'),
-          username: config.getOrThrow<string>('PG_USERNAME'),
-          password: config.getOrThrow<string>('PG_PASSWORD'),
+          host: config.get<string>('PG_HOST') || process.env.PG_HOST,
+          port: config.get<number>('PG_PORT') || Number(process.env.PG_PORT),
+          database:
+            config.get<string>('PG_DATABASE') || process.env.PG_DATABASE,
+          username:
+            config.get<string>('PG_USERNAME') || process.env.PG_USERNAME,
+          password:
+            config.get<string>('PG_PASSWORD') || process.env.PG_PASSWORD,
           autoLoadEntities: true,
           //TODO: Change to false when moving to production
-          synchronize: config.getOrThrow<boolean>('PG_SYNCHRONIZE'),
-          logging: config.getOrThrow<boolean>('PG_LOGGING'),
+          synchronize:
+            config.get<boolean>('PG_SYNCHRONIZE') ||
+            Boolean(process.env.PG_SYNCHRONIZE),
+          logging:
+            config.get<boolean>('PG_LOGGING') ||
+            Boolean(process.env.PG_LOGGING),
         };
       },
     }),
