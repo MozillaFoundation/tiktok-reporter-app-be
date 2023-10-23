d0dfc470563e19aefc62cd32656c1e5b15c8ca2c
MirceaMateiMozilla <mircea@mozillafoundation.org>
2023-10-23 11:27:30 +0300

    IP Test v2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Ip } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RealIP } from 'nestjs-real-ip';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Ip() ip): string {
-    return `${this.appService.getHello()}: ${ip}`;
+  getHello(@Ip() ip, @RealIP() realIp: string): string {
+    return `${this.appService.getHello()}: IP ** ${ip} ** RealIP: ** ${realIp} **`;
   }
 }
