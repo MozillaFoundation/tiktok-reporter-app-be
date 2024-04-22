767c54e026e2d78572623f5f577dc557cb551326
Paul Craciunoiu <paul@craciunoiu.net>
2024-04-22 17:15:01 -0600

    Replace with new bucket.

--- src/signedUrl/mappers/mapUrlToDto.ts
+++ src/signedUrl/mappers/mapUrlToDto.ts
@@ -3,7 +3,7 @@ import { SignedUrlDto } from '../dtos/signedUrl.dto';
 export function mapUrlToDto(url: string): SignedUrlDto {
   return {
     url: url.replace(
-      'https://storage.googleapis.com/regrets_reporter_recording_docs/',
+      'https://storage.googleapis.com/ttreporer_recordings/',
       '',
     ),
   } as SignedUrlDto;
