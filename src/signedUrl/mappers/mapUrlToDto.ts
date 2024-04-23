2c2c65571c7a0c559a20b88a2a63f43625d91145
Paul Craciunoiu <paul@craciunoiu.net>
2024-04-22 18:01:04 -0600

    Typo

--- src/signedUrl/mappers/mapUrlToDto.ts
+++ src/signedUrl/mappers/mapUrlToDto.ts
@@ -3,7 +3,7 @@ import { SignedUrlDto } from '../dtos/signedUrl.dto';
 export function mapUrlToDto(url: string): SignedUrlDto {
   return {
     url: url.replace(
-      'https://storage.googleapis.com/ttreporer_recordings/',
+      'https://storage.googleapis.com/ttreporter_recordings/',
       '',
     ),
   } as SignedUrlDto;
